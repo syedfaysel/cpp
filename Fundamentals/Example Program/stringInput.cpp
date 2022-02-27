09bca7f97df68b6da1172c39e10500071a00cc49
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2022-02-28 04:24:34 +0600

    String Input

---
+++ Fundamentals/Example Program/stringInput.cpp
@@ -0,0 +1,19 @@
+
+// String in C++
+
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+
+
+int main(){
+
+    char name[20];
+    cout<<"Enter your name"<<endl;
+    gets(name);
+    
+    // cin>>name;
+    cout<<"Hey,"<<name;
+
+}
